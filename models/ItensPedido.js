87e0ccd01d2755c9814a8e68412f763d4b083997
Stefani Galdino <stefanijuris@hotmail.com>
2021-05-03 23:43:19 -0300

    model ItensPedido

---
+++ models/ItensPedido.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const ItensPedido = sequelize.define(
+        'ItensPedido', {
+        valor: DataTypes.DOUBLE,
+        quantidade: DataTypes.INTEGER,
+        valor_total: DataTypes.DOUBLE,
+        pedidos_id: DataTypes.INTEGER,
+        produtos_id: DataTypes.INTEGER
+    }, {
+        tableName: 'itens_pedidos',
+        timestamps: false
+    });
+
+    ItensPedido.associate = (models) => {
+        ItensPedido.hasOne(models.Pedido, { as: "pedido", foreignKey: "pedidos_id" });
+        ItensPedido.belongsTo(models.Produto, { as: "produto", foreignKey: "produtos_id" });
+    }
+
+    return ItensPedido;
+}
