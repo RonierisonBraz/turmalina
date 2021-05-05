29cc775d69cb59e6fbca3340624eadf0d7d29551
Amanda Sousa Bezerra <amanda.sousabez@gmail.com>
2021-05-05 15:55:30 -0300

    git config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,6 @@
 module.exports = {
     "username": "root",
-<<<<<<< HEAD
-    "password": "12345",
-=======
     "password": "@FM&CS11",
->>>>>>> a799bc7e447a0c8e464e19c6d32b6dca77ac7533
     "database": "turmalina",
     "host": "127.0.0.1",
     "dialect": "mysql",
